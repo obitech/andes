1e3a4f02f809cc6a63ef237585dc732ba2711fe9
obitech <github@f0rkd.net>
2017-11-30 17:58:53 +0100

    Adjusted Vagrantfile and fixed bugs in bootstrap script

--- Vagrantfile
+++ Vagrantfile
@@ -8,25 +8,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "#{ubuntu}"
 
   # Mapping ports so we can access the frontend on our host machine
-  config.vm.network "forwarded_port", guest: 80, host: 8080
-  config.vm.network "forwarded_port", guest: 443, host: 4433
-  config.vm.network "forwarded_port", guest: 2015, host: 2015
-  config.vm.network "forwarded_port", guest: 5000, host: 5000
-
-  # Provision script
-  config.vm.provision "shell", inline: <<-SHELL
-    add-apt-repository ppa:jonathonf/python-3.6 -y
-    apt-get update -y
-    apt-get upgrade -y
-    # apt-get install vim python3.6 -y
-    # curl https://bootstrap.pypa.io/get-pip.py | sudo python3.6
-    # apt-get upgrade -y
-    # pip3 install -r /home/vagrant/andes/andes/system/app/requirements.txt
-  SHELL
-
-  # Sync this folder to ~ inside VM and disable standard syncing
-  # config.vm.synced_folder ".", "/vagrant", disabled: true
-  # config.vm.synced_folder ".", "/home/vagrant/andes"
+  config.vm.network "private_network", ip: "192.168.70.10"
 
   # VirtualBox settings
   config.vm.define :andes_dev, primary: true do |andes_dev_config|
--- andes/bootstrap.sh
+++ andes/bootstrap.sh
@@ -17,6 +17,7 @@ HOSTNAME=localhost
 ANDES_DIR=/home/$USER/andes
 COMPOSE_VERSION=1.16.1
 EMAIL=
+DOCKER_SETUP=true
 CADDY_SETUP=true
 
 echo -e "$GREEN---- Andes bootstrap script. Execute with -h to see options. $NC"
@@ -27,14 +28,15 @@ usage() {
   
   Options:
     -h                     Display this help
-    -d                     Skip Caddy install, only setup Docker
+    -c                     Skip Caddy install
+    -d                     Skip Docker install
     -H <hostname>          Specify hostname for caddy to be reached under. Defaults to localhost
     -e <email>             Your email address needed for TLS certificates. If omitted, Caddy will run without TLS
     -V <version>           Docker-compose version. Defaults to 1.16.1"
 
 }
 
-while getopts ":hdH:V:e:" o; do
+while getopts ":hdcH:V:e:" o; do
   case "${o}" in
     h )
       usage
@@ -50,6 +52,9 @@ while getopts ":hdH:V:e:" o; do
       EM=${OPTARG}
       ;;
     d )
+      DOCKER_SETUP=false
+      ;;
+    c )
       CADDY_SETUP=false
       ;;
     \? )
@@ -107,6 +112,7 @@ COMPOSE_VERSION=$COMPOSE_VERSION
 HOSTNAME=$HOSTNAME
 EMAIL=$EMAIL
 TLS_ENABLED=$TLS_ENABLED
+DOCKER_SETUP=$DOCKER_SETUP
 CADDY_SETUP=$CADDY_SETUP"
 
 echo
@@ -126,76 +132,92 @@ echo
 ############################ DOCKER INSTALLATION ############################
 #############################################################################
 
-echo -e "$GREEN==> Removing deprecated Docker versions... $NC"
-sudo apt-get remove docker docker-engine docker.io -y || true
+if [[ "$DOCKER_SETUP" = true ]]; then
+  echo -e "$GREEN==> Removing deprecated Docker versions... $NC"
+  sudo apt-get remove docker docker-engine docker.io -y || true
 
-echo -e "$GREEN==> Updating package index... $NC"
-sudo apt-get update -y
+  echo -e "$GREEN==> Updating package index... $NC"
+  sudo apt-get update -y
 
-echo -e "$GREEN==> Installing additional packages... $NC"
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common \
-    bash-completion
+  echo -e "$GREEN==> Installing additional packages... $NC"
+  sudo apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      software-properties-common \
+      bash-completion
 
-echo -e "$GREEN==> Adding docker's official GPG key... $NC"
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg \
-    | sudo apt-key add -
+  echo -e "$GREEN==> Adding docker's official GPG key... $NC"
+  curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg \
+      | sudo apt-key add -
 
-# TODO: put in Variable
-echo -e "$GREEN==> Verifying fingerprint: 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88... $NC"
-sudo apt-key fingerprint 0EBFCD88
+  # TODO: put in Variable
+  echo -e "$GREEN==> Verifying fingerprint: 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88... $NC"
+  sudo apt-key fingerprint 0EBFCD88
 
-echo -e "$GREEN==> Adding docker repository... $NC"
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-   $(lsb_release -cs) \
-   stable"
+  echo -e "$GREEN==> Adding docker repository... $NC"
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) \
+     stable"
 
-echo -e "$GREEN==> Updating package index... $NC"
-sudo apt-get update -y
+  echo -e "$GREEN==> Updating package index... $NC"
+  sudo apt-get update -y
 
-echo -e "$GREEN==> Installing docker from package manager... $NC"
-sudo apt-get install docker-ce -y
+  echo -e "$GREEN==> Installing docker from package manager... $NC"
+  sudo apt-get install docker-ce -y
 
-echo -e "$GREEN==> Creating docker group and adding user '$USER'... $NC"
-sudo groupadd docker || true
-sudo usermod -aG docker $USER
+  echo -e "$GREEN==> Creating docker group and adding user '$USER'... $NC"
+  sudo groupadd docker || true
+  sudo usermod -aG docker $USER
 
-echo -e "$GREEN==> Adding docker to systemd... $NC"
-sudo systemctl enable docker
+  echo -e "$GREEN==> Adding docker to systemd... $NC"
+  sudo systemctl enable docker
 
-echo -e "$GREEN==> Downloading docker-compose $COMPOSE_VERSION... $NC"
-sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+  echo -e "$GREEN==> Downloading docker-compose $COMPOSE_VERSION... $NC"
+  sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 
-echo -e "$GREEN==> Adding docker-compose to bash-completion...$NC"
-sudo curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose -o /usr/share/bash-completion/completions/docker-compose
+  echo -e "$GREEN==> Adding docker-compose to bash-completion...$NC"
+  sudo curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose -o /usr/share/bash-completion/completions/docker-compose
 
-echo -e "$GREEN==> Testing hello-world...$NC"
-sudo docker run hello-world
+  echo -e "$GREEN==> Testing hello-world...$NC"
+  sudo docker run hello-world
 
-echo -e "$GREEN==> Docker installed successfully, source .bashrc for bash-completion! $NC"
+  echo -e "$GREEN==> Docker installed successfully, source .bashrc for bash-completion! $NC"
+fi
 
 #####################################################################
 ############################ CADDY SETUP ############################
 #####################################################################
-
-if [[ CADDY_SETUP ]]; then
+  
+if [[ "$CADDY_SETUP" = true ]]; then
+  
   # Changing hostname
   if [[ "$HOSTNAME" != "localhost" ]]; then
     sed -i "s/localhost/$HOSTNAME/g" ./system/Caddyfile
+    echo -e "$GREEN==> Caddyfile: Hostname has been changed to $HOSTNAME. $NC"
   fi
 
   # Setting TLS
   # TODO: fix this
-  if [[ EMAIL != '' ]]; then
+  if [[ "$EMAIL" ]]; then
     sed -i "s/tls off/tls $EMAIL/g" ./system/Caddyfile
+    echo -e "$GREEN==> Caddy: tls Email has been changed to $EMAIL. $NC"
   fi
 
+  echo -e "$GREEN==> Caddyfile has been saved:$NC"
   cat ./system/Caddyfile
+
+  read -p "Do you want to start Andes now (Y/n)? " choice
+  case "$choice" in 
+    n|N|no|No )
+      echo -e "$RED==> Aborting."
+      exit 1
+      ;; 
+    * )
+      ;;
+  esac
 
   echo -e "$GREEN==> Starting Andes...$NC"
   sudo docker-compose up -d
