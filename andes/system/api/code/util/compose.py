4409f3b0124dca49c9dee622f59661991827680a
obitech <github@f0rkd.net>
2017-12-14 10:41:32 +0100

    Docstringed util functions

--- andes/system/api/code/util/compose.py
+++ andes/system/api/code/util/compose.py
@@ -1,17 +1,28 @@
 def get_compose(data):
-  """
-  Data should look like this:
-  data = [ 
-    service_1 = {
-      name: '...',
-      image: '...',
-      exposed_ports: [...],
-      mapped_ports: [...],
-      volumes: [...],
-      environment: [...]
-    },
-    service_n = { ... }
-  ]
+  """Function to render the docker-compose.yml for a specific stack.
+  
+  This will use the docker-compose.yml template specified in the loader path.
+
+  Note:
+    Data should look like this::
+      data = [ 
+        service_1 = {
+          name: '...',
+          image: '...',
+          exposed_ports: [...],
+          mapped_ports: [...],
+          volumes: [...],
+          environment: [...]
+        },
+        service_n = { ... }
+      ]
+
+  Args:
+    data (:obj:`dict`): A dictionary with the necessary data in it.
+
+  Returns:
+    str: The a string containing the docker-compose information, according to the passed data.
+      None if there has been an exception.
   """
   from jinja2 import Environment, FileSystemLoader
   from os import path, getcwd
@@ -32,6 +43,15 @@ def get_compose(data):
     return None
 
 def create_compose(path, text):
+  """Helper function to write the docker-compose string to file.
+
+  Args:
+    text (str): The docker-compose information as a string.
+
+  Returns:
+    bool: True if writing was successful, None if not.
+
+  """
   from traceback import print_exc
   try:
     with open(path, 'w') as data_file:
--- andes/system/api/code/util/response.py
+++ andes/system/api/code/util/response.py
@@ -1,4 +1,16 @@
 def response(status, message, error, data):
+  """A helper function to format a proper API response.
+
+  Args:
+    status (int): The HTTP status code.
+    message (str): A message which should be displayed.
+    error (str): An error message incase the status code is not 2xx.
+    data (optional): The data payload. Should be null, a list or a dict.
+  
+  Returns:
+    dict: A dictionary with the passed data.
+
+  """
   return {
     'status': status,
     'message': message,
