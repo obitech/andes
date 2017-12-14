35c8e08dab81bf3bcc3bb1e717a11cd2d353c235
obitech <github@f0rkd.net>
2017-12-14 10:44:15 +0100

    Docstringed security.py

--- andes/system/api/code/security.py
+++ andes/system/api/code/security.py
@@ -1,10 +1,29 @@
 from models.user import UserModel
 
 def authenticate(username, password):
+  """Function handling user authentication
+
+  Args:
+    username (str): The user to authenticate
+    password (str): The password of the user
+
+  Returns:
+    A user object if authentication was successful, None if not.
+
+  """
   user = UserModel.find_by_username(username)
   if user and user.check_password(password):
     return user
 
 def identity(payload):
+  """Function retrieving user ID during authentication
+
+  Args:
+    payload (:obj:`dict`): A dictionary with authentication details
+
+  Returns:
+    A user ID or None if user wasn't found.
+    
+  """
   user_id = payload['identity']
   return UserModel.find_by_id(user_id)
